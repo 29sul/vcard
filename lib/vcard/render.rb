6051267b40459f6d3a0d8ad133a9ec55a0d3e860
Elton Luis Kolling <eltonlk@gmail.com>
2015-06-29 15:22:02 -0300

    Adicionado suporte ao rails 4.2.

--- lib/vcard/render.rb
+++ lib/vcard/render.rb
@@ -1,5 +1,3 @@
-Mime::Type.register 'text/x-vcard', :vcf
-
 ActionController::Renderers.add :vcf do |object, options|
   self.content_type ||= 'text/x-vcard'
   self.response_body  = object.respond_to?(:to_vcf) ? object.to_vcf : object
